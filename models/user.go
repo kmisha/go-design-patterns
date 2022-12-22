450b1e7914e8bc5ca683ae31364b3b1d89e5deff
Mikhail Kolesnikov <dadyaaya@gmail.com>
2022-12-22 18:43:14 +0400

    add user entity

---
+++ models/user.go
@@ -0,0 +1,14 @@
+package models
+
+import "github.com/google/uuid"
+
+type User struct {
+	ID       uuid.UUID
+	Name     string
+	Surname  string
+	Fullname string
+}
+
+func NewUser(name, surename string) User {
+	return User{uuid.New(), name, surename, name + " " + surename}
+}
